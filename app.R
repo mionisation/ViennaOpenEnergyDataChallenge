5efb5feba60b7af653e98aee206fc0ae6d38268e
Michael Ion <michael.antonius.ion@gmail.com>
2017-02-14 16:52:43 +0000

    add second visualization for total cost

--- app.R
+++ app.R
@@ -1,22 +1,37 @@
+library(ggplot2)
+library(ggthemes)
+library(shinythemes)
 ## begin global
 
-lang_ger <- c("Vergleich von Benutzung öffentlicher Verkehrsmittel vs. PKW in Wien", "Mobilitätscheck",
+lang_ger <- c("Vergleich der Benutzung öffentlicher Verkehrsmittel vs. PKW in Wien", "Mobilitätscheck",
               "Wähle Sprache", "Details der Auswahl", 
               "Gesamttransportation", "Jahr",
-              "Preis in Euro", "Liter pro 100 Kilometer",
+              "Preis in Euro", "Oder: Liter pro 100 Kilometer angeben:",
               'Art der Kosten', 'Kosten im Jahr',
               'Kosten am Tag', 'Öffentlich',
               'Auto', 'Art des Transports',
-              'Preis: ', "Jahr: ")
+              'Preis: ', "Jahr: ",
+              'Selektierte Jahre: ', "Transport pro Person",
+              "Oder: Anzahl der Autofahrer in Wien angeben:", "Verwende totale PKW-Zulassungen in Wien",
+              "Verwende Durchschnittsverbrauch", " Millionen Euro",
+              "Preis in Millionen Euro")
+              #23
 lang_eng <- c("Comparison of car usage vs. public services in Vienna", "Mobility check",
               "Choose language", "Detailed look in this selection",
               "Plot of transportation details", "Year",
-              "Price in Euro", "Liters per 100 Kilometers",
+              "Price in Euro", "Or: Specify Liters per 100 Kilometers: ",
               "Types of Cost", "Cost per Year",
               "Cost per day", "Public",
               "Car", "Type of transport",
-              "Price: ", "Year: ")
+              "Price: ", "Year: ",
+              "Selected years: ", "Transport per person",
+              "Or: Specify car drivers in Vienna:", "Use total amount of licensed cars in Vienna",
+              "Use average consumption of all cars", " million Euro",
+              "Price in million Euro")
 lang <- lang_ger
+
+# data from the energy report and other sources specified in the README.
+# Missing values were estimated via linear and loess regression
 
 transportationData <- data.frame (
   year = c(2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016),
@@ -24,6 +39,8 @@ transportationData <- data.frame (
   wl_cardsPrice = c(417, 417, 485, 485, 485, 485, 485, 365, 365, 365, 365, 365),
   car_kmPerDay = c(36.06,	36.06,	34.7,	34.7,	35.09,	35.09,	32.82,	32.82,	30.23,	30.23, 30, 30),
   car_PricePerL = c(0.83,	0.89,	0.93,	1.09,	0.90,	1.06,	1.28,	1.38,	1.35,	1.32,	1.22, 1.17),
+  car_consumptionPer100km = c(7.4, 7.4, 7.5, 7.5, 7.3, 7.3, 7.3, 7.3, 7.1, 7.1, 7.1, 7.1),
+  car_numbers = c( 655806, 658081, 657426, 657192, 663926, 669279, 674526, 679492, 681413, 683258, 685570, 687243),
   na = c(NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA)
 )
 
@@ -54,19 +71,34 @@ server <- function(input, output) {
   mainPlotData <- reactive({
     publicMultiplier = 1
     carMultiplier = 1
+    #adjust data if selection is cost per year or per day
     if(input$costType == lang[10]) {
       carMultiplier = 365
     } else {
       publicMultiplier = 1/365
     }
+    #adjust if selection is custom value for number of cars or fuel consumption
+    carNumber = input$carDrivers
+    fuelConsumption = input$LitersPerKm
+    if(input$useFuelStatisticValues) {
+      fuelConsumption = transportationData$car_consumptionPer100km
+    }
+    if(input$useCarStatisticValues) {
+      carNumber = transportationData$car_numbers
+    }
+    
+    carPricePerPerson = carMultiplier * transportationData$car_PricePerL * transportationData$car_kmPerDay * (fuelConsumption/100)
     main <- data.frame (
       year = transportationData$year,
       publicPrice = transportationData$wl_cardsPrice * publicMultiplier,
-      carPrice =  carMultiplier * transportationData$car_PricePerL * transportationData$car_kmPerDay * (input$LitersPerKm/100)
+      carPrice =  carPricePerPerson,
+      publicPriceTotal = transportationData$wl_cardsPrice * publicMultiplier * transportationData$wl_cardsSold/1000000,
+      carPriceTotal = carNumber * carPricePerPerson / 1000000
     )
     
   })
   
+  ##start text output for hovering / brushing
   output$selectYear <- renderText({
     yearFrom <- input$mainBrush$xmin
     yearTo <- input$mainBrush$xmax
@@ -74,9 +106,8 @@ server <- function(input, output) {
       yearFrom <- 2005
       yearTo <- 2016
     }
-    paste0("Years selected: ", round(yearFrom), " to ", round(yearTo))
+    paste0(lang[17], round(yearFrom), " - ", round(yearTo))
   })
-  
   output$hoverYear <- renderText({
     val <- input$plot_hover$x
     if(is.null(val)) {
@@ -84,7 +115,6 @@ server <- function(input, output) {
     }
     paste0(lang[16], round(val))
   })
-  
   output$hoverPrice <- renderText({
      val <- input$plot_hover$y
      if(is.null(val)) {
@@ -92,6 +122,31 @@ server <- function(input, output) {
      }
      paste0(lang[15], round(val, digits = 2), " Euro")
   })
+  output$selectYearTotal <- renderText({
+    yearFrom <- input$sideBrush$xmin
+    yearTo <- input$sideBrush$xmax
+    if(is.null(yearFrom)) {
+      yearFrom <- 2005
+      yearTo <- 2016
+    }
+    paste0(lang[17], round(yearFrom), " - ", round(yearTo))
+  })
+  output$hoverYearTotal <- renderText({
+    val <- input$plot_hover_total$x
+    if(is.null(val)) {
+      val <- 0
+    }
+    paste0(lang[16], round(val))
+  })
+  output$hoverPriceTotal <- renderText({
+    val <- input$plot_hover_total$y
+    if(is.null(val)) {
+      val <- 0
+    }
+    paste0(lang[15], round(val, digits = 2), lang[22])
+  })
+  ##end text output for hovering / brushing
+  
 
   output$mainPlot <- renderPlot({
     m <- mainPlotData()
@@ -102,7 +157,25 @@ server <- function(input, output) {
       geom_point(data=m, aes(x = year, y = carPrice, color = lang[13]), size=3, fill="white") + 
       labs(color=lang[14]) +
       xlab(lang[6]) +
-      ylab(lang[7])
+      ylab(lang[7]) + 
+      expand_limits(y=0) +
+      theme_solarized_2(light = TRUE) +
+      scale_colour_solarized("blue")
+  })
+  
+  output$sidePlot <- renderPlot({
+    m <- mainPlotData()
+    ggplot() +
+      geom_line(data=m, aes(x = year, y = publicPriceTotal, color = lang[12]), size=.75 ) +
+      geom_point(data=m, aes(x = year, y = publicPriceTotal, color = lang[12]), size=3, fill="white") +
+      geom_line(data=m, aes(x = year, y = carPriceTotal, color = lang[13]), size=.75) +
+      geom_point(data=m, aes(x = year, y = carPriceTotal, color = lang[13]), size=3, fill="white") + 
+      labs(color=lang[14]) +
+      xlab(lang[6]) +
+      ylab(lang[23]) + 
+      expand_limits(y=0) +
+      theme_solarized_2(light = TRUE) +
+      scale_colour_solarized("blue")
   })
 }
 ## end server
@@ -110,23 +183,43 @@ server <- function(input, output) {
 
 ## begin UI
 
-ui <- fluidPage(
+ui <- fluidPage(theme = shinytheme("flatly"),
+                tags$head(
+                  tags$style(HTML("
+                    @import url('//fonts.googleapis.com/css?family=Lobster|Cabin:400,700');
+                    body {
+                      background-color: #fdf6e3;;
+                    }
+                    h2 {
+                      font-family: 'Lobster', cursive;
+                      font-weight: 500;
+                      line-height: 1.1;
+                    }
+                    .checkbox label span{
+                      font-weight: bold;
+                    }
+                  "))
+  ),
   
   # Application title
   titlePanel(lang[1], windowTitle = lang[2]),
   
   # Sidebar with a slider input for number of bins 
   fluidRow(
-    column( width = 3,
+    column( width = 2,
             #selectInput('langSel', lang[3], languageSelection, selected = "Deutsch"),
+            radioButtons(inputId = 'costType', label = lang[9], choices = c(lang[10], lang[11])),
+            helpText("_"),
+            checkboxInput(inputId = 'useFuelStatisticValues', label = lang[21], value = TRUE),
             sliderInput(inputId = 'LitersPerKm', label = lang[8], min = 2, max = 10, value = 8, step = 0.5 ),
-            selectInput(inputId = 'costType', label = lang[9], choices = c(lang[10], lang[11])),
-            helpText(""),
-            helpText("")
+            helpText("_"),
+            checkboxInput(inputId = 'useCarStatisticValues', label = lang[20], value = TRUE),
+            sliderInput(inputId = 'carDrivers', label = lang[19], min = 0, max = 800000, value = 680000, step = 10000 )
     ),
     
     # Show a plot of the generated distribution
-    column( width = 6,
+    column( width = 5,
+            helpText(lang[18]),
             plotOutput(outputId = "mainPlot",
                        brush = brushOpts(
                           id = "mainBrush",
@@ -140,16 +233,38 @@ ui <- fluidPage(
                          delayType = 'throttle',
                          nullOutside = FALSE
                        )
-            ),     
-            helpText(lang[5])
+            ),    
+            helpText(lang[4]),
+            textOutput("hoverYear"),
+            textOutput("hoverPrice"),
+            textOutput("selectYear")
     ),
     
-    column( width = 3,
+    column( width = 5,
     #todo: include here selection plots
+    helpText(lang[5]),
+    plotOutput(outputId = "sidePlot",
+               brush = brushOpts(
+                 id = "sideBrush",
+                 delayType = 'debounce',
+                 delay = 200,
+                 direction = 'x'
+               ),
+               hover = hoverOpts(
+                 id = "plot_hover_total",
+                 delay = 200,
+                 delayType = 'throttle',
+                 nullOutside = FALSE
+               )
+    ), 
     helpText(lang[4]),
-    textOutput("hoverYear"),
-    textOutput("hoverPrice"),
-    textOutput("selectYear")
+    textOutput("hoverYearTotal"),
+    textOutput("hoverPriceTotal"),
+    textOutput("selectYearTotal"),
+    plotOutput(outputId = "fuelPrice"),
+    plotOutput(outputId = "ticketPrice")
+    #include: how much money/co2 saved compared to how many car owners drive per day
+    #slider: select number of drivers: from 0 to 500.000
     ),
     
     position = "right",
